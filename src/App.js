427b50636ff010ff76c451541b9a79949c929346
Jonathan Britton <jbritttt29@hotmail.com>
2022-12-16 02:15:13 +0000

    form with datepicker added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import Expenses from "./components/Expenses/Expenses";
-
+import NewExpense from "./components/NewExpense/NewExpense";
 function App() {
   const expenses = [
     {
@@ -25,6 +25,7 @@ function App() {
 
   return (
     <div>
+      <NewExpense/>
       <Expenses items={expenses}/>
     </div>
   );
--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,3 +1,6 @@
+import { useState } from 'react'
+
+
 import "./ExpenseItem.css";
 
 import ExpenseDate from "./ExpenseDate";
@@ -5,15 +8,27 @@ import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 
 function ExpenseItem(props) {
+
+console.log('expense item evaluated by react')
+const [title, setTitle] = useState(props.title)
+
+
+const clickHandler = () =>{
+
+  setTitle('updated')
+  console.log(title)
+}
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
 
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
 
         <div className="expense-item__price">£{props.amount}</div>
       </div>
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 }
