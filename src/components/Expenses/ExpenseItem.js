c6579d978f41ae282349f0f58527f03d96145178
Jonathan Britton <jbritttt29@hotmail.com>
2022-12-16 13:02:35 +0000

    removed test button

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,6 +1,3 @@
-import { useState } from 'react'
-
-
 import "./ExpenseItem.css";
 
 import ExpenseDate from "./ExpenseDate";
@@ -8,27 +5,17 @@ import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 
 function ExpenseItem(props) {
-
-console.log('expense item evaluated by react')
-const [title, setTitle] = useState(props.title)
-
-
-const clickHandler = () =>{
-
-  setTitle('updated')
-  console.log(title)
-}
+  console.log("expense item evaluated by react");
 
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
 
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
 
         <div className="expense-item__price">£{props.amount}</div>
       </div>
-      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 }
