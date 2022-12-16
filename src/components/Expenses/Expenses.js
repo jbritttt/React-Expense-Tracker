7a947d882797492055dbf7b4e52d675dccd99670
Jonathan Britton <jbritttt29@hotmail.com>
2022-12-16 14:29:56 +0000

    added functionality for dropdown filter

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -12,6 +12,11 @@ function Expenses(props) {
     setFilteredYear(selectedYear);
   };
 
+  const filteredExpenses = props.items.filter(expense => {
+return expense.date.getFullYear().toString() === filteredYear
+
+  })
+
   return (
     <div>
       <Card className="expenses">
@@ -19,7 +24,7 @@ function Expenses(props) {
           selected={filteredYear}
           onChangeFilter={filterChangeHandler}
         />
-        {props.items.map((expense) => (
+        {filteredExpenses.map((expense) => (
           <ExpenseItem
           key={expense.id}
             title={expense.title}
